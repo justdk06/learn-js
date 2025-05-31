754b0a7510ba423b1d2e6c0ceac7f950734d1e4e
Dharmendra Kumar <136125899+justdk06@users.noreply.github.com>
2025-05-31 12:45:55 +0000

    commit push

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -54,3 +54,30 @@ console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: 'js in hindi',
+    price: '999',
+    courseInstructor: 'Dharmendra'
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     'name': 'dharmendra',
+//     'coursename': 'Js in hindi',
+//     'price': 'free'
+// }
+
+[
+    {},
+    {},
+    {}
+]
