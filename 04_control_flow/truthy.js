2a0f7a3cdbf0a716a74ee3ff7255c6c549d8a145
Dharmendra Kumar <136125899+justdk06@users.noreply.github.com>
2025-06-01 14:03:02 +0000

    truthy.js commit

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = []
+// const userEmail = []
 
-if (userEmail) {
-    console.log('Got user email');
-} else {
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log('Got user email');
+// } else {
+//     console.log("Don't have user email");
+// }
 
 
 // falsy values
@@ -14,12 +14,30 @@ if (userEmail) {
 // truthy values
 // '0', 'false', " ", [], {}, function(){}
 
-if (userEmail.length === 0){
-    console.log('Array is empty');
-}
+// if (userEmail.length === 0){
+//     console.log('Array is empty');
+// }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0){
-   console.log('Object is empty');
-}
+// if (Object.keys(emptyObj).length === 0){
+//    console.log('Object is empty');
+// }
+
+// Nullish Coalescing Operator ((??)): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1)
+
+// Terniary Operator
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log('less than 80'): console.log('more than 80')
+
+
+// lecture ... 26 complete
