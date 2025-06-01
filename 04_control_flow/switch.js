e289cb5a3db3753c210c172be45d602c7570b423
Dharmendra Kumar <136125899+justdk06@users.noreply.github.com>
2025-06-01 13:55:51 +0000

    commit all 3

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key){
+//     case Value:
+//         break;
+
+//     default:
+//         break;
+
+// }
+
+const month = 'March'
+
+switch (month) {
+    case 'January':
+        console.log('January')
+        break;
+        
+    case 'February':
+        console.log('February')
+        break;
+    case 'March':
+        console.log('March')
+        break;
+        
+    case 'April':
+        console.log('April')
+        break;
+
+    default:
+        console.log('default case match');
+        break;
+
+}
